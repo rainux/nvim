809d1d72de8473a639ec643b06dece5c7e710393
Rainux Luo <rainux@gmail.com>
2022-03-18 07:19:29 +0800

    Configure lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,3 +1,38 @@
+-- Helper function  {{{1
+vim.cmd([[
+function! GetCharCode()
+  " Get the output of :ascii
+  redir => ascii
+  silent! ascii
+  redir END
+
+  if match(ascii, 'NUL') != -1
+    return 'NUL'
+  endif
+
+  " Zero pad hex values
+  let nrformat = '0x%02x'
+
+  let encoding = (&fenc == '' ? &enc : &fenc)
+
+  if encoding == 'utf-8'
+    " Zero pad with 4 zeroes in unicode files
+    let nrformat = '0x%04x'
+  endif
+
+  " Get the character and the numeric value from the return value of :ascii
+  " This matches the two first pieces of the return value, e.g.
+  " "<F>  70" => char: 'F', nr: '70'
+  let [str, char, nr; rest] = matchlist(ascii, '\v\<(.{-1,})\>\s*([0-9]+)')
+
+  " Format the numeric value
+  let nr = printf(nrformat, nr)
+
+  return "'". char ."' ". nr
+endfunction
+]])
+-- }}}1
+
 require('lualine').setup({
   options = {
     icons_enabled = true,
@@ -11,7 +46,7 @@ require('lualine').setup({
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
     lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_x = { 'GetCharCode', 'filesize', 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
   },
@@ -24,5 +59,7 @@ require('lualine').setup({
     lualine_z = {},
   },
   tabline = {},
-  extensions = {},
+  extensions = { 'fugitive', 'nvim-tree', 'quickfix', 'symbols-outline' },
 })
+
+-- vim: fdm=marker fdl=0 tw=120:
