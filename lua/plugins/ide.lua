7185c827efd001086e71740fadb41b2962c2c3d8
Rainux Luo <rainux@gmail.com>
2024-02-19 21:05:41 +0800

    Migrate conform and lint

---
+++ lua/plugins/ide.lua
@@ -0,0 +1,30 @@
+return {
+  -- Lightweight yet powerful formatter plugin for Neovim
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform will run multiple formatters sequentially
+        python = { 'isort', 'black', 'ruff' },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { 'prettierd', 'prettier' } },
+        ruby = { 'rubyfmt', 'rubocop' },
+      },
+    },
+  },
+  -- An asynchronous linter plugin for Neovim complementary to the built-in Language Server Protocol support.
+  {
+    'mfussenegger/nvim-lint',
+    opts = {
+      linters_by_ft = {
+        go = { 'golangcilint' },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        python = { 'ruff' },
+        ruby = { 'ruby', 'rubocop' },
+        markdown = { 'vale' },
+      },
+    },
+  },
+}
