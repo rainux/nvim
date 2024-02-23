fc9aa5a733f45cfb47c413d0d8a271c9b5bf2114
Rainux Luo <rainux@gmail.com>
2024-02-23 13:45:18 +0800

    Disable plugin updates check on startup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,7 @@ require('lazy').setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { 'tokyonight', 'habamax' } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- It's so fucking crazy to automatically check for plugin updates on startup
   performance = {
     rtp = {
       -- disable some rtp plugins
